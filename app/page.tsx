299b554a5108d3dfd3c2fc11991689433e02af90
unknown <jkturner107@gmail.com>
2025-06-12 19:36:18 +0700

    added readme

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,9 @@ import Link from "next/link";
 export default function Home() {
 	return (
 		<div className="flex flex-col w-full max-w-[1280px] h-[60vh] justify-center items-center mx-auto">
-			<h1 className="font-semibold text-lg mb-4">React JS Map Method Tutorial</h1>
-			<p className="opacity-70 mb-4 w-[400px] text-center">Practice how to use the .filter() method with live React examples from simple conditions to dynamic dropdown and checkbox filters.</p>
+			<h1 className="font-semibold text-lg mb-4">React JS Find Method Tutorial</h1>
+			<p className="opacity-70 mb-4 w-[400px] text-center">Practice how to use the .find() method with live React examples from simple value checks to searching within nested project data.
+</p>
 			<div className="flex gap-4">
 				<Link href="/simple">
 					<button className="bg-teal-200 py-2 px-4 text-slate-800 cursor-pointer hover:opacity-70">
